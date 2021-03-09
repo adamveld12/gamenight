004a3366b498fc8c10079c17ddfe7ff4c5d627bd
Adam Veldhousen <adamveld12@gmail.com>
2021-03-08 22:16:31 -0600

    more updates

--- build.sh
+++ build.sh
@@ -5,15 +5,17 @@ function build(){
   local imageName=$2;
 
   echo "Building ${imageName}"
-  docker build --build-arg steam_user=${STEAMUSER} --build-arg steam_pass=${STEAMPASS} -t ${imageName} ./${buildDir}
-  docker push ${imageName}
+  docker build --build-arg STEAM_USER=${STEAMUSER} --build-arg STEAM_PASS=${STEAMPASS} -t ${imageName} ./${buildDir}
+  docker push -q ${imageName}
 }
 
 TAG="1.2.0"
 
+build ./steamcmd "gamenight/steamcmd:${TAG}";
+
 for D in */; do
-  echo "~~~~~BUILDING $D~~~~~~"
-  build $D "gamenight/${D%?}:${TAG}" &
+    echo "~~~~~BUILDING $D~~~~~~"
+    build $D "gamenight/${D%?}:${TAG}" &
 done;
 
 time wait;
