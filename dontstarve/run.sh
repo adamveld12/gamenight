08c855ad9ecb104819848644e871849ec2b1eba1
Adam Veldhousen <adamveld12@gmail.com>
2021-03-08 23:10:03 -0600

    fix file stuff

--- dontstarve/run.sh
+++ dontstarve/run.sh
@@ -2,5 +2,5 @@
 
 cd /games/${APPID}/bin
 exec /games/${APPID}/bin/dontstarve_dedicated_server_nullrenderer \
-    -conf_dir /data/configs \
-    -persistent_storage_root /data/saves $@
+    -persistent_storage_root /data/saves \
+    -conf_dir ../configs $@
