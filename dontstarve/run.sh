afd7574120b5bb763af2795c3a36282ab560ee90
Adam Veldhousen <adamveld12@gmail.com>
2021-03-09 01:07:42 -0600

    f

--- dontstarve/run.sh
+++ dontstarve/run.sh
@@ -2,5 +2,5 @@
 
 cd /games/${APPID}/bin
 exec /games/${APPID}/bin/dontstarve_dedicated_server_nullrenderer \
-    -persistent_storage_root /data/saves \
-    -conf_dir ../configs $@
+    -persistent_storage_root /data \
+    -conf_dir ./configs $@
