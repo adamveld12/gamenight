daa6df14b5ac1d0c5bcc582e53b996edd48d5852
Adam Veldhousen <adamveld12@gmail.com>
2021-03-22 22:37:12 -0500

    updated minecraft and dontstarve

--- build.sh
+++ build.sh
@@ -12,11 +12,11 @@ function build(){
 TAG="1.2.0"
 
 build ./steamcmd "gamenight/steamcmd:${TAG}";
-build ./dontstarve "gamenight/dontstarve:${TAG}" &;
-build ./minecraft "gamenight/minecraft:1.16.5" &;
-build ./minecraft "gamenight/minecraft:1.16.5-paper" Dockerfile.paper &;
-build ./terraria "gamenight/terraria:1353" &;
-build ./factorio "gamenight/factorio:stable" &;
+build ./dontstarve "gamenight/dontstarve:${TAG}" &
+build ./minecraft "gamenight/minecraft:1.16.5" &
+build ./minecraft "gamenight/minecraft:1.16.5-paper" Dockerfile.paper &
+build ./terraria "gamenight/terraria:1353" &
+build ./factorio "gamenight/factorio:stable" &
 
 time wait;
 
