b9f85b7b5dc10de081a0f7f2f5f6758fc3492d9a
Adam Veldhousen <adamveld12@gmail.com>
2021-03-23 02:23:16 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -2,28 +2,34 @@
 function build() {
   local buildDir=$1;
   local imageName=$2;
-  local df=$3;
+  local tag=$3;
+  local df=$4;
 
-  if [ -z "${df}" ]; then
-    dockerfile = "Dockerfile";
+  if [ -z "$df" ]; then
+    df="Dockerfile";
   fi
 
-  echo "Building ${imageName}"
-  docker build --build-arg STEAM_USER=${STEAMUSER} --build-arg STEAM_PASS=${STEAMPASS} \
-              -t ${imageName} \
-              -f "${df}" \
-              ./${buildDir}
+  echo -e "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nBuilding '${imageName}'\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+  docker build --build-arg "STEAM_USER=${STEAMUSER}" --build-arg "STEAM_PASS=${STEAMPASS}" \
+              -t "${imageName}:${tag}" \
+              -f "${buildDir}/${df}" \
+              ${buildDir};
 
-  docker push -q ${imageName}
+  if [ "${BRANCH}" = "master" ]; then
+    docker tag "${imageName}:${tag}" "${imageName}:latest";
+  fi
+
+  docker push -a -q ${imageName};
 }
 
 TAG="1.2.1"
-build ./steamcmd "gamenight/steamcmd:${TAG}";
-build ./dontstarve "gamenight/dontstarve:${TAG}" &
-build ./minecraft "gamenight/minecraft:1.16.5" &
-build ./minecraft "gamenight/minecraft:1.16.5-paper" Dockerfile.paper &
-build ./terraria "gamenight/terraria:1353" &
-build ./factorio "gamenight/factorio:stable" &
+build ./steamcmd "gamenight/steamcmd" "${TAG}";
+build ./dontstarve "gamenight/dontstarve" "${TAG}" &
+build ./minecraft "gamenight/minecraft" "1.16.5" &
+build ./minecraft "gamenight/minecraft" "1.16.5-paper" "Dockerfile.paper" &
+build ./terraria "gamenight/terraria" "1353" &
+build ./factorio "gamenight/factorio" "stable" &
+build ./rust "gamenight/rust" "stable" &
 
 time wait;
 
