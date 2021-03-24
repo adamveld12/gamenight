cdf7848c38759e7d92693a394c42365befe86d28
Adam Veldhousen <adamveld12@users.noreply.github.com>
2021-03-24 01:28:01 -0500

    add valheim (#5)
    * add valheim
    
    * updated file paths

---
+++ valheim/run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export LD_LIBRARY_PATH=./linux64:$LD_LIBRARY_PATH
+export SteamAppID=892970
+
+export ispub="0"
+if [ -z "${VH_PUBLIC}" ]; then
+    ispub="1"
+fi
+
+echo "Starting server PRESS CTRL-C to exit"
+./valheim_server.x86_64 -port 2456 -nographics -batchmode \
+-savedir /data \
+-name "${VH_SERVER_NAME}" \
+-world "${VH_WORLD}" \
+-password "${VH_PASSWORD}" \
+-public "${ispub}"
