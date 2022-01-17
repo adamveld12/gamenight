36edb0fe15d068a58848c1a5b09146216887dfc5
Adam Veldhousen <adamveld12@gmail.com>
2022-01-16 21:45:51 -0600

    factorio updates

--- factorio/create-save.sh
+++ factorio/create-save.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-export GAME_SAVE_PATH=/data/saves/${DEFAULT_SAVE_FILE}.zip
+export GAME_SAVE_PATH=/data/saves/${SAVE_FILE:-world}.zip
 
 if [[ -f /data/configs/map-gen-settings.json ]]; then
-    /games/factorio/bin/x64/factorio --create ${GAME_SAVE_PATH} --map-gen-settings /data/configs/map-gen-settings.json
+    /games/factorio/bin/x64/factorio --create ${GAME_SAVE_PATH} \
+                                     --map-gen-settings /data/configs/map-gen-settings.json
 else
     /games/factorio/bin/x64/factorio --create ${GAME_SAVE_PATH} $@
 fi
--- factorio/run.sh
+++ factorio/run.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
+export GAME_SAVE_PATH=/data/saves/${SAVE_FILE:-world}.zip
 
-docker run -it -v $PWD/_data/saves:/data/saves -v $PWD/_data/configs:/data/configs --entrypoint bash adamveld12/factorio
+if ! [ -f "${GAME_SAVE_PATH}" ]; then 
+    create-save.sh;
+fi
+
+mkdir -p '/data/logs';
+
+if [ -z "$(cat /etc/group | grep ${PGID})"  ]; then
+    addgroup -g ${PGID} factorio;
+fi
+
+if [ -z "$(cat /etc/passwd | grep ${PUID})"  ]; then
+    adduser -u ${PUID} -G factorio -D -h /data factorio;
+fi
+
+chown -R ${PUID}:${PGID} /data;
+chown -R ${PUID}:${PGID} /games;
+
+exec su factorio -c "/games/factorio/bin/x64/factorio --verbose \
+                                 --start-server \"${GAME_SAVE_PATH}\" \
+                                 --server-id /data/server_id \
+                                 --console-log /data/logs/console.log \
+                                 --mod-directory /data/mods \
+                                 --map-gen-settings /data/configs/map-gen-settings.json \
+                                 --map-settings /data/configs/map-settings.json \
+                                 --server-settings /data/configs/server-settings.json"
