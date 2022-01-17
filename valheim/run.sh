285b3e6d0619d433c22f1fb43f5e868666e07461
Adam Veldhousen <adamveld12@gmail.com>
2022-01-17 16:12:23 -0600

    fix: update valheim

--- valheim/run.sh
+++ valheim/run.sh
@@ -10,12 +10,13 @@ if [[ -z "${VH_SERVER_PASSWORD}" ]]; then
     _PASSWORD_ARG="-password"
 fi
 
+PUSER=valheim;
 if [ -z "$(cat /etc/group | grep ${PGID})"  ]; then
-    addgroup --gid ${PGID} valheim;
+    addgroup -q --gecos "" --gid ${PGID} ${PUSER};
 fi
 
 if [ -z "$(cat /etc/passwd | grep ${PUID})"  ]; then
-    adduser --uid ${PUID} --gid ${PGID} --disabled-password --disabled-login --home /data valheim;
+    adduser -q --gecos "" --uid ${PUID} --gid ${PGID} --disabled-password --disabled-login --no-create-home ${PUSER};
 fi
 
 chown -R ${PUID}:${PGID} /data;
@@ -24,8 +25,8 @@ chown -R ${PUID}:${PGID} /games;
 exec su valheim -c "export LD_LIBRARY_PATH=\"./linux64:$LD_LIBRARY_PATH\"; \
                     export SteamAppID=892970; \
                     echo \"Starting Valheim (\${SteamAppID}) server\"; \
-                    cd /games/${APPID}; \
-                    /games/${APPID}/valheim_server.x86_64 -nographics -batchmode \
+                    cd /games/valheim; \
+                    /games/valheim/valheim_server.x86_64 -nographics -batchmode \
                                                           -savedir /data \
                                                           -port 2456 \
                                                           -name \"${VH_SERVER_NAME}\" ${_PASSWORD_ARG}  \
