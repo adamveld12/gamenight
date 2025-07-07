79c27e56992b7a07c336a69eaee00014ecd6c3b6
Adam Veldhousen <adamveld12@gmail.com>
2025-07-07 10:31:00 -0500

    update minecraft

--- minecraft/run.sh
+++ minecraft/run.sh
@@ -5,25 +5,14 @@ cat <<EOF > /data/eula.txt
 eula=true
 EOF
 
-mkdir -p '/data/configs';
-mkdir -p '/data/world';
-mkdir -p '/data/plugins';
+mkdir -p '/data/configs' '/data/world' '/data/plugins';
 
-if [ -f /configs/server.properties ]; then
-    cp /configs/server.properties /data/server.properties;
-fi
+chown -R minecraft:minecraft /data;
 
-PUSER=minecraft
-if [ -z "$(cat /etc/group | grep ${PGID})"  ]; then
-    addgroup -g ${PGID} ${PUSER};
-fi
-
-if [ -z "$(cat /etc/passwd | grep ${PUID})"  ]; then
-    adduser -u ${PUID} -G ${PUSER} -D -h /data ${PUSER};
-fi
+CONFIG_FILES=$(find /configs -type f -name '*.properties' -o -name '*.yml')
+for file in ${CONFIG_FILES}; do
+    cp -vf "${file}" /data/configs/;
+done
 
 
-chown -R ${PUID}:${PGID} /data;
-chown -R ${PUID}:${PGID} /games;
-
-exec su ${PUSER} -c "java -Xmx${JVM_MEMORY_SIZE} -Xms512m ${JAVA_OPTS} -jar /games/minecraft/server.jar --nogui"
+su minecraft -c "java -Xmx${JVM_MEMORY_SIZE} -Xms512m ${JAVA_OPTS} -jar /games/minecraft/server.jar --nogui"
